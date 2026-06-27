4c235437a785b44bd161aa807afd0011e4a0f8de
luiss-a11y <luis.s@jcacontadores.com.br>
2026-06-26 21:29:02 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // FutWord Service Worker
 // Bump CACHE_VERSION whenever you deploy changes so clients fetch the new files.
-const CACHE_VERSION = "futword-v3";
+const CACHE_VERSION = "futword-v4";
 const CORE_ASSETS = [
   "./",
   "./index.html",
